a305a110e9d4256e782d88651d0b9586939b500d
Ming <github@programmingbasics.org>
2021-02-23 15:36:22 -0500

    Adding in structs needed for fetch and push in Android

--- libgit2/android/libgit2-overlay/src/android_jni.c
+++ libgit2/android/libgit2-overlay/src/android_jni.c
@@ -59,3 +59,41 @@ void releaseJvmEnv(void)
     if (envFromAttach)
         (*gJvm)->DetachCurrentThread(gJvm);
 }
+
+
+// Some extra functions for interfacing with libgit2
+
+GIT_EXTERN(int) git_fetch_options_size(void) {
+  return sizeof(git_fetch_options);
+}
+
+GIT_EXTERN(int) git_push_options_size(void) {
+  return sizeof(git_push_options);
+}
+
+GIT_EXTERN(int) git_status_options_size(void) {
+  return sizeof(git_status_options);
+}
+
+GIT_EXTERN(int) git_fetch_options_version(void) {
+  return GIT_FETCH_OPTIONS_VERSION;
+}
+
+GIT_EXTERN(int) git_push_options_version(void) {
+  return GIT_PUSH_OPTIONS_VERSION;
+}
+
+GIT_EXTERN(int) git_status_options_version(void) {
+  return GIT_STATUS_OPTIONS_VERSION;
+}
+
+GIT_EXTERN(void) git_status_options_config(git_status_options * opts, const char **path) {
+  if (path != NULL) {
+    opts->pathspec.count = 1;
+    opts->pathspec.strings = path;
+  }
+  opts->show = GIT_STATUS_SHOW_INDEX_AND_WORKDIR;
+	opts->flags = GIT_STATUS_OPT_INCLUDE_UNTRACKED |
+		GIT_STATUS_OPT_RECURSE_UNTRACKED_DIRS |
+		GIT_STATUS_OPT_INCLUDE_UNMODIFIED;
+}
