8f36da6608fcde00016600af6ece68340d2eb2ed
Ming <github@programmingbasics.org>
2021-03-10 17:57:42 -0500

    Need to adjust more names of things to fit the new package naming

---
+++ android/app/src/main/java/dev/plom/plomgit/MainActivity.java
@@ -0,0 +1,7 @@
+package dev.plom.plomgit;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity 
+{
+}
--- android/app/src/main/kotlin/com/example/PlomGit/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.PlomGit
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
